45bc0bef6ae068732aa5d6c84a250099e6b7921b
digitaldd <digital-dd@mail.ru>
2018-04-08 07:16:15 +0300

    1.3. JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate Решение задачи 1.3 JavaDoc. Code convention
+*
+* @author Mikhail Kochin (digital-dd@mail.ru)
+* @since 08.04.2018
+*/
 public class Calculate {
+
+/**
+* this method out to screen Helloword
+*/
     public static void main(String[] args) {
         System.out.println("HelloWorld");
     }
