27801c6e788ba5c0dd5590425b11d73f3a6c52fd
Егор Долганов <e.dolganov@newbtec.ru>
2018-04-10 10:12:33 +0300

    4.2. Создать программу вычисляющую факториал

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+/**
+ * Class Factorial Решение задачи 4.2. Создать программу вычисляющую факториал
+ *
+ * @author Mikhail Kochin (digital-dd@mail.ru)
+ * @since 10.04.2018
+ */
+public class Factorial {
+
+    /**
+     * calculate factorial number (finish)
+     *
+     * @param finish last number in cycle
+     * @return factorial of number (finish)
+     */
+    public int calculateFactorial(int finish) {
+        int result = 1;
+        if (finish >= 0) {
+            for (int i = finish; i >= 1; i--) {
+                result *= i;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * Class FactorialTest Решение задачи 4.2. Создать программу вычисляющую факториал
+ *
+ * @author Mikhail Kochin (digital-dd@mail.ru)
+ * @since 10.04.2018
+ */
+public class FactorialTest {
+
+    /**
+     * test factorial number 5
+     */
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+        Factorial fact = new Factorial();
+        assertThat(fact.calculateFactorial(5), is(120));
+        //напишите здесь тест, проверяющий, что факториал для числа 5 равен 120.
+    }
+
+    /**
+     * test factorial number 0
+     */
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        Factorial fact = new Factorial();
+        assertThat(fact.calculateFactorial(0), is(1));
+        //напишите здесь тест, проверяющий, что факториал для числа 0 равен 1.
+    }
+}
