5cf837cbf484e670004364e1029d0aaf959fe365
digitaldd <digital-dd@mail.ru>
2018-04-23 11:27:31 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора

--- chapter_002/src/main/java/tracker/start/ValidateInput.java
+++ chapter_002/src/main/java/tracker/start/ValidateInput.java
@@ -1,18 +1,30 @@
 package tracker.start;
 
 /**
- * Class ValidateInput Решение задачи 1. Обеспечить бесперебойную работу приложения Tracker
+ * Class ValidateInput Решение задачи 2. Рефакторинг - Шаблон Декоратор для валидатора
  *
  * @author Mikhail Kochin (digital-dd@mail.ru)
  * @since 23.04.2018
  */
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+
+    private final Input input;
+
+    public ValidateInput(final Input input) {
+        this.input = input;
+    }
+
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
+
     public int ask(String question, int[] range) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
             } catch (MenuOutException moe) {
                 System.out.println("Please select key from menu.");
