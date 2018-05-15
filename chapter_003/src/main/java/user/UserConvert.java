e1b2dfc1a210c27fa4f9c33bf20efc4ecfb4c8a0
digitaldd <digital-dd@mail.ru>
2018-05-15 20:02:44 +0300

    2. Написать программу преобразования List в Map (fixed)

--- chapter_003/src/main/java/user/UserConvert.java
+++ chapter_003/src/main/java/user/UserConvert.java
@@ -1,6 +1,5 @@
 package user;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,15 +10,6 @@ import java.util.List;
  * @since 15.05.2018
  */
 public class UserConvert {
-    public static void main(String[] args) {
-        User user1 = new User(1, "Fred", "Fili");
-        User user2 = new User(2, "Sam", "Orlando");
-        List<User> userList = new ArrayList<>();
-        userList.add(user1);
-        userList.add(user2);
-        UserConvert uc = new UserConvert();
-    }
-
     public HashMap<Integer, User> process(List<User> list) {
         HashMap<Integer, User> resultMap = new HashMap<>();
         for (User user : list) {
