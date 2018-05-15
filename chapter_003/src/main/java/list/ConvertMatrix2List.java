ee77b10038c2129d830d811bcf45d942a563b0d6
digitaldd <digital-dd@mail.ru>
2018-05-15 14:58:29 +0300

    1. Конвертация листа массивов в один лист Integer

--- chapter_003/src/main/java/list/ConvertMatrix2List.java
+++ chapter_003/src/main/java/list/ConvertMatrix2List.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 /**
  * Class ConvertMatrix2List Решение задачи 3. Конвертация двумерного массива в ArrayList
+ * 1. Конвертация листа массивов в один лист Integer (convert)
  *
  * @author Mikhail Kochin (digital-dd@mail.ru)
  * @since 15.05.2018
@@ -18,5 +19,15 @@ public class ConvertMatrix2List {
             }
         }
         return list;
+    }
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> newListInteger = new ArrayList<>();
+            for (int[] i : list) {
+            for (int j : i) {
+                newListInteger.add(j);
+            }
+        }
+        return newListInteger;
     }
 }
--- chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -3,6 +3,7 @@ package ru.job4j.list;
 import list.ConvertMatrix2List;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -66,6 +67,45 @@ public class ConvertMatrix2ListTest {
                 1, 2, 3, 4, 5, 6, 7, 8
         );
         List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * test convert list with arrays to list Integer (2 arrays and 5 elements)
+     */
+    @Test
+    public void when2With5ElementsThenList5() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[] input = {1, 2};
+        int[] input2 = {3, 4, 5};
+        List<int[]> la = new ArrayList<>();
+        la.add(input);
+        la.add(input2);
+        List<Integer> result = list.convert(la);
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5
+        );
+        assertThat(result, is(expect));
+    }
+    /**
+     * test convert list with arrays to list Integer (4 arrays and 10 elements)
+     */
+    @Test
+    public void when4With10ElementsThenList10() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[] input = {1, 2};
+        int[] input2 = {3, 4, 5};
+        int[] input3 = {6, 7, 8};
+        int[] input4 = {9, 10};
+        List<int[]> la = new ArrayList<>();
+        la.add(input);
+        la.add(input2);
+        la.add(input3);
+        la.add(input4);
+        List<Integer> result = list.convert(la);
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        );
         assertThat(result, is(expect));
     }
 }
