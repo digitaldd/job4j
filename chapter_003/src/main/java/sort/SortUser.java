1eeb9dabf57221eaa56d0851819f994205e2d559
digitaldd <digital-dd@mail.ru>
2018-05-17 16:51:56 +0300

    1. Организовать сортировку User (fixed)

--- chapter_003/src/main/java/sort/SortUser.java
+++ chapter_003/src/main/java/sort/SortUser.java
@@ -11,12 +11,6 @@ import java.util.*;
 public class SortUser {
 
     public Set<User> sort(List<User> list) {
-        list.sort(new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return o1.compareTo(o2);
-            }
-        });
         return new TreeSet<>(list);
     }
 
