eccb7e60fb17cb165857b27a4ea6299adbc78d9a
digitaldd <digital-dd@mail.ru>
2018-07-16 15:29:32 +0300

    1. Создать модель User

---
+++ Junior/part_001/src/main/java/ru.job4j/Map/User.java
@@ -0,0 +1,45 @@
+package ru.job4j.Map;
+
+import java.util.Calendar;
+
+/**
+ * Class User Решение задачи 1. Создать модель User
+ *
+ * @author Mikhail Kochin (digital-dd@mail.ru)
+ * @since 13.07.2018
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+}
