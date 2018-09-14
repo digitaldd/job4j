d80add8f8239059f7059768795336930bedf9641
digitaldd <digital-dd@mail.ru>
2018-09-14 17:00:09 +0300

    Обеспечить остановку потребителя

---
+++ Junior/part_002/src/main/java/ru.job4j/wait/SimpleBlockingQueueWait.java
@@ -0,0 +1,44 @@
+package ru.job4j.wait;
+
+/**
+ * Class SimpleBlockingQueueWait Решение задачи 2. Обеспечить остановку потребителя
+ *
+ * @author Mikhail Kochin (digital-dd@mail.ru)
+ * @since 14.09.2018
+ */
+public class SimpleBlockingQueueWait {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        );
+        producer.start();
+    }
+}
